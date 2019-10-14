edbd7f1bde8e6a88b9855f0171630c8acd6ff26d
Tetiana <tatyananalezhata43@gmail.com>
2019-10-14 09:19:57 +0300

    pull

---
+++ app/src/main/java/com/example/auth/tetiana/LoginActivity.java
@@ -0,0 +1,129 @@
+package com.example.auth.tetiana;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class LoginActivity extends AppCompatActivity {
+    EditText Email, Password;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    String email, password;
+    ProgressDialog dialog;
+    Button LogIn;
+    FirebaseAuth.AuthStateListener mAuthListner;
+
+
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Email = (EditText) findViewById(R.id.editEmail);
+        Password = (EditText) findViewById(R.id.editPassword);
+        LogIn = (Button) findViewById(R.id.buttonLogin);
+
+        TextView createNewUser = (TextView)findViewById(R.id.buttonRegister);
+        dialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userSign();
+            }
+        });
+
+
+        createNewUser.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.removeAuthStateListener(mAuthListner);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListner != null) {
+            mAuth.removeAuthStateListener(mAuthListner);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+        setIntent.addCategory(Intent.CATEGORY_HOME);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(setIntent);
+    }
+
+
+    private void userSign() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Email.setError("Enter Email");
+            Email.setFocusable(true);
+            return;
+        }else if (TextUtils.isEmpty(password)){
+            Password.setError("Enter Password");
+            Password.setFocusable(true);
+            return;
+        }
+
+        dialog.setIndeterminate(true);
+        dialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    dialog.dismiss();
+                    Password.setError("Invalid Password");
+                    Password.setFocusable(true);
+                    return;
+                } else {
+                    dialog.dismiss();
+                    Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+
+    }
+
